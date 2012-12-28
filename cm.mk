f5e8008a7c1b7891da881d87dbb0c37daf027f88
invisiblek <dan.pasanen@gmail.com>
2012-12-27 21:28:20 -0600

    d2usc: update fingerprint for LK5
    Change-Id: I842da29d87ed27879343720d13fcdf9844151caf

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2usc TARGET_DEVICE=d2usc BUILD_FINGERPRINT="d2usc-user 4.0.4 IMM76D R530UVXALG1 release-keys" PRIVATE_BUILD_DESC="samsung/d2usc/d2usc:4.0.4/IMM76D/R530UVXALG1:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2usc TARGET_DEVICE=d2usc BUILD_FINGERPRINT="d2usc-user 4.1.1 JRO03L R530UVXALK5 release-keys" PRIVATE_BUILD_DESC="samsung/d2usc/d2usc:4.1.1/JRO03L/R530UVXALK5:user/release-keys"
 
 PRODUCT_NAME := cm_d2usc
 PRODUCT_DEVICE := d2usc
