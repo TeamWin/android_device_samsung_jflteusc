99b1b73125fb0b7fd4859519759f17bc7f5c0e7e
Shareef Ali <shareefalis@cyanogenmod.org>
2013-05-26 23:58:24 -0500

    fix rename

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 ## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/jfr530/jfr530-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/jflteusc/jflteusc-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/jflteusc/overlay
