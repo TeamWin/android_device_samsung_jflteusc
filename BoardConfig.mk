af0da2219675da6e78086c8a2a932a1471547f90
Shareef Ali <shareefalis@cyanogenmod.org>
2013-06-04 15:06:24 -0500

    jflteusc: fix naming derpiness
    Change-Id: If36bdf2f81c30eb86fe40aacf7674843260bc8d9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@
 -include device/samsung/jf-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
--include vendor/samsung/jfr530/BoardConfigVendor.mk
+-include vendor/samsung/jflteusc/BoardConfigVendor.mk
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := jflteusc
