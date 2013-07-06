570082a60718e2496846164b37373f2729c2c273
Shareef Ali <shareefalis@cyanogenmod.org>
2013-07-05 22:25:29 -0500

    jf: general fixes
    Change-Id: Ib520728e453e0167d44e74df2428b0517313f855

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/jflter970/jflter970-vendor.mk)
 
+# Disable MSB for GPS
+NEEDS_GPS_MSB_DISABLED := true
+
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/jflteusc/overlay
 ## common overlays
