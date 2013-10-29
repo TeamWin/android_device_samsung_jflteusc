70357e308deabe90b3a26dd133fbf50eb8cd8ef5
Dan Pasanen <dan.pasanen@gmail.com>
2013-10-28 21:33:35 -0500

    jflteusc: remove cdma.mk
    Change-Id: I841df360e64736592954ebd10c6819f09e8a61a6

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/jflteusc/full_jflteusc.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
